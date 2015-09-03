d874d6f8508b0af12424ffe663f39dbc5aee82e0
Vy-Shane Sin Fat <shane@node.mu>
2015-09-03 22:48:08 +0800

    Ensure that all k8s-started containers are removed on kube-down.

--- kube-down.sh
+++ kube-down.sh
@@ -2,15 +2,5 @@
 cd kubernetes
 docker-compose stop
 docker-compose rm -f
-
-echo "Removing scheduler..."
-docker rm -f `docker ps -a -f "name=k8s_scheduler." -q`
-
-echo "Removing controller manager.."
-docker rm -f `docker ps -a -f "name=k8s_controller-manager." -q`
-
-echo "Removing API server..."
-docker rm -f `docker ps -a -f "name=k8s_apiserver." -q`
-
-echo "Removing pods..."
-docker rm -f `docker ps -a -f "name=k8s_POD." -q`
+echo "Removing k8s-started containers..."
+docker rm -f `docker ps -a -f "name=k8s_" -q`
