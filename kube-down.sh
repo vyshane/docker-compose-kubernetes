887f93480bde6b1378a820c8cdf2ecb359651090
Vy-Shane Sin Fat <shane@node.mu>
2015-09-04 12:33:03 +0800

    Fixed my crappy bash scripting.

--- kube-down.sh
+++ kube-down.sh
@@ -5,7 +5,7 @@ docker-compose rm -f
 
 k8s_containers=`docker ps -a -f "name=k8s_" -q`
 
-if [ ! -z $k8s_containers ]; then
+if [ ! -z "$k8s_containers" ]; then
     echo "Removing k8s-started containers..."
     docker rm -f $k8s_containers
 fi
