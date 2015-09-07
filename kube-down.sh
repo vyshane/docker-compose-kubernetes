046e6a4189cbaa4dafd82fd324a51de8d48680da
Vy-Shane Sin Fat <shane@node.mu>
2015-09-07 11:16:26 +0800

    Gracefully stop replication controllers, services and pods before removing Docker containers. Remove volumes too when removing Docker containers.

--- kube-down.sh
+++ kube-down.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "Stopping replaction controllers, services and pods..."
+kubectl stop replicationcontrollers,services,pods --all
+
 cd kubernetes
 docker-compose stop
 docker-compose rm -f
@@ -7,5 +10,5 @@ k8s_containers=`docker ps -a -f "name=k8s_" -q`
 
 if [ ! -z "$k8s_containers" ]; then
     echo "Removing k8s-started containers..."
-    docker rm -f $k8s_containers
+    docker rm -f -v $k8s_containers
 fi
