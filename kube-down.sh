c40a0b72b5b75f32b2bef2318f1a4e0d18130980
Vy-Shane Sin Fat <shane@node.mu>
2015-09-07 12:48:34 +0800

    Also remove volumes when calling rm with docker-compose.

--- kube-down.sh
+++ kube-down.sh
@@ -4,11 +4,12 @@ kubectl stop replicationcontrollers,services,pods --all
 
 cd kubernetes
 docker-compose stop
-docker-compose rm -f
+docker-compose rm -f -v
 
 k8s_containers=`docker ps -a -f "name=k8s_" -q`
 
 if [ ! -z "$k8s_containers" ]; then
-    echo "Removing k8s-started containers..."
+    echo "Stopping and removing all other containers that were started by Kubernetes..."
+    docker stop $k8s_containers
     docker rm -f -v $k8s_containers
 fi
