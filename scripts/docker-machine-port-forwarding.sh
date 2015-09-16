d7d77a08fe8668933229343bc279049609363ae1
Vy-Shane Sin Fat <shane@node.mu>
2015-09-16 23:46:16 +0800

    Only enable port forwarding if it's not currently active.

--- scripts/docker-machine-port-forwarding.sh
+++ scripts/docker-machine-port-forwarding.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+#
+# Set up kubectl port forwarding to boot2docker VM if needed.
 
-# First remove any existing forwards
-ps aux | grep 'ssh -f -N -L 8080:localhost:8080 docker@' | grep -v grep | awk '{print $2}' | xargs kill
+forward_port_command="ssh -f -N -L 8080:localhost:8080 docker@$(docker-machine ip $(docker-machine active))"
+existing_forward=$(ps ax | grep "$forward_port_command" | grep -v grep)
 
-# Set up port forwarding so that we can use kubectl locally
-ssh -f -N -L 8080:localhost:8080 docker@$(docker-machine ip $(docker-machine active))
+if [ -z "$existing_forward" ]; then
+    eval $forward_port_command
+fi
