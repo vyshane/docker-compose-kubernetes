9bb7d5fb90c4ecf4f2a91c6def9910243a471af8
Oliver Moser <o@omoser.com>
2015-10-15 10:52:38 +0200

    merging changes from upstream

--- scripts/docker-machine-port-forwarding.sh
+++ scripts/docker-machine-port-forwarding.sh
@@ -4,15 +4,16 @@
 
 source ../.settings
 
-machine=$(docker-machine active)
-keyfile="$HOME/.docker/machine/machines/$machine/id_rsa"
-
 function forward_port_if_not_forwarded {
     port=$1
+
+    machine=$(docker-machine active)
+    keyfile="$HOME/.docker/machine/machines/$machine/id_rsa"
+
 	printf "${yellow}Checking forward for port '$port'${reset}\n"
     forward_port_command="ssh -i $keyfile -f -N -L $port:localhost:$port docker@$(docker-machine ip $machine)"
     existing_forward=$(ps ax | grep "$forward_port_command" | grep -v grep)
-    
+
     if [ -z "$existing_forward" ]; then
 		#printf "${yellow}Adding portforward for port '$port'${reset}\n"
         eval $forward_port_command &> /dev/null
